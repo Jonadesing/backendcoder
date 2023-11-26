12d389fdab59afd22688c58281fe6348135266f1
Jonadesing <109099372+Jonadesing@users.noreply.github.com>
2023-11-26 11:17:39 -0300

    entrega N^2

--- productManager.js
+++ productManager.js
@@ -1,60 +1,97 @@
+const fs = require('fs');
+
 class ProductManager {
-    constructor() {
-      this.products = [];
-      this.productId = 1; // ID autoincrementable
-    }
-  
-    addProduct(title, description, price, thumbnail, code, stock) {
-      // Validar campos obligatorios
-      if (!title || !description || !price || !thumbnail || !code || !stock) {
-        throw new Error('Todos los campos son obligatorios.');
-      }
-  
-      // Validar código único
-      const codeExists = this.products.some((product) => product.code === code);
-      if (codeExists) {
-        throw new Error('El código ya existe. Debe ser único.');
-      }
-  
-      const newProduct = {
-        id: this.productId,
-        title,
-        description,
-        price,
-        thumbnail,
-        code,
-        stock,
-      };
-  
-      this.products.push(newProduct);
-      this.productId++; // Incrementar el ID para el próximo producto
-  
-      return newProduct;
-    }
-  
-    getProducts() {
-      return this.products;
-    }
-  
-    getProductById(id) {
-      const foundProduct = this.products.find((product) => product.id === id);
-      if (foundProduct) {
-        return foundProduct;
-      } else {
-        throw new Error('Producto no encontrado.');
-      }
+  constructor(path) {
+    this.path = path;
+    this.products = [];
+    this.readDataFromFile();
+  }
+
+  readDataFromFile() {
+    try {
+      const data = fs.readFileSync(this.path, 'utf8');
+      this.products = JSON.parse(data);
+    } catch (error) {
+      console.error('Error al leer el archivo:', error);
     }
   }
-  
-  // Crear instancia de ProductManager
-  const manager = new ProductManager();
-  
-  // Llamar a getProducts, debería devolver un arreglo vacío
-  console.log(manager.getProducts());
-  
-  // Agregar un nuevo producto
+
+  async saveDataToFile() {
+    try {
+      await fs.promises.writeFile(this.path, JSON.stringify(this.products, null, 2));
+    } catch (error) {
+      console.error('Error al guardar en el archivo:', error);
+    }
+  }
+
+  async addProduct(title, description, price, thumbnail, code, stock) {
+    // Validar campos obligatorios
+    if (!title || !description || !price || !thumbnail || !code || !stock) {
+      throw new Error('Todos los campos son obligatorios.');
+    }
+
+    // Validar código único
+    const codeExists = this.products.some((product) => product.code === code);
+    if (codeExists) {
+      throw new Error('El código ya existe. Debe ser único.');
+    }
+
+    const newProduct = {
+      id: this.products.length > 0 ? Math.max(...this.products.map((p) => p.id)) + 1 : 1,
+      title,
+      description,
+      price,
+      thumbnail,
+      code,
+      stock,
+    };
+
+    this.products.push(newProduct);
+    await this.saveDataToFile();
+    return newProduct.id;
+  }
+
+  getProducts() {
+    return this.products;
+  }
+
+  getProductById(id) {
+    const foundProduct = this.products.find((product) => product.id === id);
+    if (foundProduct) {
+      return foundProduct;
+    } else {
+      throw new Error('Producto no encontrado.');
+    }
+  }
+
+  async updateProduct(id, updatedFields) {
+    const productIndex = this.products.findIndex((product) => product.id === id);
+    if (productIndex !== -1) {
+      this.products[productIndex] = { ...this.products[productIndex], ...updatedFields };
+      await this.saveDataToFile();
+      return true;
+    }
+    return false;
+  }
+
+  async deleteProduct(id) {
+    const initialLength = this.products.length;
+    this.products = this.products.filter((product) => product.id !== id);
+    if (this.products.length !== initialLength) {
+      await this.saveDataToFile();
+      return true;
+    }
+    return false;
+  }
+}
+
+// Ejemplo de uso:
+const productManager = new ProductManager('products.json');
+
+(async () => {
+  // Agregar un producto
   try {
-    const newProduct = manager.addProduct(
+    const productId = await productManager.addProduct(
       'producto prueba',
       'Este es un producto prueba',
       200,
@@ -62,33 +99,36 @@ class ProductManager {
       'abc123',
       25
     );
-    console.log('Producto agregado:', newProduct);
+    console.log('Producto agregado con ID:', productId);
   } catch (error) {
     console.log('Error al agregar el producto:', error.message);
   }
-  
-  // Volver a llamar a getProducts, debería mostrar el producto agregado anteriormente
-  console.log(manager.getProducts());
-  
-  // Intentar agregar un producto con el mismo código
+
+  // Obtener todos los productos
+  const allProducts = productManager.getProducts();
+  console.log('Todos los productos:', allProducts);
+
+  // Obtener un producto por ID
   try {
-    manager.addProduct(
-      'producto prueba',
-      'Este es un producto prueba',
-      200,
-      'Sin imagen',
-      'abc123',
-      25
-    );
-  } catch (error) {
-    console.log('Error al agregar el producto:', error.message);
-  }
-  
-  // Probar getProductById con un ID válido y otro inválido
-  try {
-    console.log(manager.getProductById(1)); // Producto existente
-    console.log(manager.getProductById(3)); // Producto no existente
+    const productById = productManager.getProductById(1);
+    console.log('Producto con ID 1:', productById);
   } catch (error) {
     console.log('Error:', error.message);
   }
-  
+
+  // Actualizar un producto
+  const updated = await productManager.updateProduct(1, { price: 150, stock: 25 });
+  if (updated) {
+    console.log('Producto actualizado exitosamente');
+  } else {
+    console.log('No se pudo actualizar el producto');
+  }
+
+  // Eliminar un producto por ID
+  const deleted = await productManager.deleteProduct(1);
+  if (deleted) {
+    console.log('Producto eliminado exitosamente');
+  } else {
+    console.log('No se pudo eliminar el producto');
+  }
+})();
